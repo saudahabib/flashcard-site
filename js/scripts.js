3d6e2e058ddd254ba7041cec6ce61ad69408e1fa
saudahabib <saudababs00@gmail.com>
2019-02-20 16:10:28 +0300

    Add toggle feature to all elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 alert(5)
 $(document).ready(function()  {
-  $("h3").click(function()  {
-    $("#show1").show();
+  $(".click-me").click(function()  {
+    $("#show1").toggle();
+    $("#hide1").toggle();
+  });
+  $(".click-me").click(function(){
+    $("#show2").toggle();
+    $("#hide2").toggle();
+  });
+  $(".click-me").click(function()  {
+    $("#show3").toggle();
+    $("#hide3").toggle();
   });
 });
