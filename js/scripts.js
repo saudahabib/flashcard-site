99e0e7ceb1a95ea76341e295b3bf0e5cea24c023
saudahabib <saudababs00@gmail.com>
2019-02-20 16:53:46 +0300

    Make each part independent

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 alert(5)
 $(document).ready(function()  {
-  $(".click-me").click(function()  {
+  $(".click-me1").click(function()  {
     $("#show1").toggle();
     $("#hide1").toggle();
   });
-  $(".click-me").click(function(){
+  $(".click-me2").click(function(){
     $("#show2").toggle();
     $("#hide2").toggle();
   });
-  $(".click-me").click(function()  {
+  $(".click-me3").click(function()  {
     $("#show3").toggle();
     $("#hide3").toggle();
   });
